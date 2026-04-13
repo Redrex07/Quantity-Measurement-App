ef708212ee2bb2dc675a8bda3c277b1d3d3611e9
Your Name <rohnaguchhait2007@gmail.com>
2026-04-13 15:44:01 +0530

    UC1: Feet Measurement Equality implemented

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,2 +1,25 @@
 public class QuantityMeasurementApp {
+
+    // Method to check equality of two feet values
+    public static boolean isEqual(double value1, double value2) {
+
+        // Validation (numeric check)
+        if (Double.isNaN(value1) || Double.isNaN(value2)) {
+            throw new IllegalArgumentException("Invalid input: Not a number");
+        }
+
+        // Equality check
+        return Double.compare(value1, value2) == 0;
+    }
+
+    public static void main(String[] args) {
+
+        // UC1 Inputs (Feet)
+        double feet1 = 5.0;
+        double feet2 = 5.0;
+
+        boolean result = isEqual(feet1, feet2);
+
+        System.out.println("Feet Equality: " + result);
+    }
 }
